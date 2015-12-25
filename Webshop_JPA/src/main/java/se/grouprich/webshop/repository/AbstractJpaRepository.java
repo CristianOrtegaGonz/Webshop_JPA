cfa519f2b3aee9f63162f07c8fd1e2923fefa0b2
lindabjalla <lindabjalla@gmail.com>
2015-12-25 15:11:40 +0100

    skapat AbstractJpaRepository class

---
+++ Webshop_JPA/src/main/java/se/grouprich/webshop/repository/AbstractJpaRepository.java
@@ -0,0 +1,87 @@
+package se.grouprich.webshop.repository;
+
+import java.util.List;
+import java.util.function.Function;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
+import se.grouprich.webshop.model.AbstractEntity;
+
+public abstract class AbstractJpaRepository<E extends AbstractEntity> implements CrudRepository<E>
+{
+	private final EntityManagerFactory factory;
+	private final Class<E> entityClass;
+
+	protected AbstractJpaRepository(EntityManagerFactory factory, Class<E> entityClass)
+	{
+		this.factory = factory;
+		this.entityClass = entityClass;
+	}
+
+	public E findById(Long id)
+	{
+		EntityManager manager = factory.createEntityManager();
+		try
+		{
+			return manager.find(entityClass, id);
+		}
+		catch (PersistenceException e)
+		{
+			throw new RuntimeException(e);
+		}
+		finally
+		{
+			manager.close();
+		}
+	}
+
+	public List<E> fetchAll(String queryName, Function<TypedQuery<E>, TypedQuery<E>> queryFunction)
+	{
+		EntityManager manager = factory.createEntityManager();
+		try
+		{
+			TypedQuery<E> typedQuery = manager.createNamedQuery(queryName, entityClass);
+			return queryFunction.apply(typedQuery).getResultList();
+		}
+		catch (PersistenceException e)
+		{
+			throw new RuntimeException(e);
+		}
+		finally
+		{
+			manager.close();
+		}
+	}
+
+	public E saveOrUpdate(E entity)
+	{
+		return entity.getId() == null ? execute(manager -> {
+			manager.persist(entity);
+			return entity;
+		}) : execute(manager -> manager.merge(entity));
+	}
+
+	protected E execute(Function<EntityManager, E> operation)
+	{
+		EntityManager manager = factory.createEntityManager();
+		try
+		{
+			manager.getTransaction().begin();
+			E result = operation.apply(manager);
+			manager.getTransaction().commit();
+
+			return result;
+		}
+		catch (PersistenceException e)
+		{
+			throw new RuntimeException(e);
+		}
+		finally
+		{
+			manager.close();
+		}
+	}
+}
--- Webshop_JPA/src/main/java/se/grouprich/webshop/repository/CrudRepository.java
+++ Webshop_JPA/src/main/java/se/grouprich/webshop/repository/CrudRepository.java
@@ -1,12 +1,15 @@
 package se.grouprich.webshop.repository;
 
 import java.util.List;
+import java.util.function.Function;
+
+import javax.persistence.TypedQuery;
 
 import se.grouprich.webshop.model.AbstractEntity;
 
 public interface CrudRepository<E extends AbstractEntity>
 {
 	 E findById(Long id);
-	 List<E> getAll();
+	 List<E> fetchAll(String queryName, Function<TypedQuery<E>, TypedQuery<E>> queryFunction);
 	 E saveOrUpdate(E entity);
 }
