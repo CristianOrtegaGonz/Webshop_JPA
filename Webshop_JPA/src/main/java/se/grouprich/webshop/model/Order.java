9004efdd0899e4cdb542cf683940eedcf989ce25
Josefine <josefine.lantz@aventyret.com>
2015-12-26 08:37:53 +0100

    Första ändringar för att se om det jag tänkt går att implementera. Ser dock att logik är lite annan, så vi behöver nog prata ihop oss...

--- Webshop_JPA/src/main/java/se/grouprich/webshop/model/Order.java
+++ Webshop_JPA/src/main/java/se/grouprich/webshop/model/Order.java
@@ -1,7 +1,10 @@
 package se.grouprich.webshop.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import se.grouprich.webshop.exception.OrderException;
 import se.grouprich.webshop.exception.PaymentException;
 import se.grouprich.webshop.idgenerator.Identifiable;
 
@@ -9,18 +12,93 @@ public final class Order implements Serializable, Identifiable<String>
 {
 	private static final long serialVersionUID = 3380539865925002167L;
 	private String orderId;
-	private final ShoppingCart shoppingCart;
-	private Customer customer;
+	private final User shopper;
+	private List<OrderRow> orderRows;
+	private double orderTotal;
 	private boolean isPayed;
 
-	public Order(String orderId, Customer customer, ShoppingCart shoppingCart)
+	public Order(String orderId, User shopper, List<OrderRow> orderRows)
 	{
 		this.orderId = orderId;
-		this.customer = customer;
-		this.shoppingCart = shoppingCart;
+		this.shopper = shopper;
+		this.orderRows = orderRows;
+		caluculateOrderPrice();
 		isPayed = false;
 	}
-	
+
+    public double getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void addToOrder(Product product, int quantity) {
+        orderRows.add(new OrderRow(product, quantity));
+    }
+
+    public void addToOrder(Product product) {
+        orderRows.add(new OrderRow(product));
+    }
+
+    public void addToOrder(List<OrderRow> orderRows) {
+        this.orderRows = orderRows;
+    }
+
+    public void setOrderTotal(double orderTotal) {
+        this.orderTotal = orderTotal;
+    }
+
+    public String getShopperUserName() {
+        return shopper.getUsername();
+    }
+
+    public User getOrderShopper() {
+        return this.shopper;
+    }
+
+    public List<OrderRow> getOrderRows() {
+        return new ArrayList<>(orderRows);
+    }
+
+	public double calculateTotalPrice()
+	{
+		totalPrice = 0;
+		for (Product product : products)
+		{
+			totalPrice += (product.getPrice() * product.getOrderQuantity());
+		}
+		return totalPrice;
+	}
+
+	public void addProductInShoppingCart(final Product product, final int orderQuantity) throws OrderException
+	{
+		products.add(product);
+		product.setOrderQuantity(orderQuantity);
+		calculateTotalPrice();
+	}
+
+	public void deleteOneProduct(final Product product) throws OrderException
+	{
+		if (!products.contains(product))
+		{
+			throw new OrderException("Product doesn't exsists.");
+		}
+		products.remove(product);
+		calculateTotalPrice();
+	}
+
+	public void emptyShoppingCart(final List<Product> products)
+	{
+		products.removeAll(products);
+		calculateTotalPrice();
+	}
+
+	public void calculateOrderPrice(){
+        double total = 0.0;
+        for(OrderRow orderRow : orderRows){
+            total += orderRow.getItem().getPrice() * orderRow.getQuantity();
+        }
+        this.orderTotal = total;
+    }
+	 */
 	@Override
 	public String getId()
 	{
@@ -33,14 +111,10 @@ public final class Order implements Serializable, Identifiable<String>
 		this.orderId = orderId;
 	}
 
-	public ShoppingCart getShoppingCart()
-	{
-		return shoppingCart;
-	}
 	
-	public Customer getCustomer()
+	public User getShopperUserName()
 	{
-		return customer;
+		return shopper.getUsername();
 	}
 
 	public boolean isPayed()
@@ -48,44 +122,35 @@ public final class Order implements Serializable, Identifiable<String>
 		return isPayed;
 	}
 	
-	public void pay() throws PaymentException
+	/*public void pay() throws PaymentException
 	{
 		isPayed = true;
 		for (Product product : shoppingCart.getProducts())
 		{
 			product.setStockQuantity(product.getStockQuantity() - product.getOrderQuantity());
 		}
-	}
+	}*/
 
 	@Override
-	public boolean equals(Object other)
-	{
-		if (this == other)
-		{
-			return true;
-		}
-
-		if (other instanceof Order)
-		{
+	public boolean equals(Object other) {
+		if (this == other) return true;
+		if (this instanceof Order) {
 			Order otherOrder = (Order) other;
-			return orderId == otherOrder.orderId && shoppingCart.equals(otherOrder.shoppingCart);
+			return orderId.equals(otherOrder.getId()) && shopper.getId().equals(otherOrder.getOrderShopper().getId());
 		}
 		return false;
 	}
 
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		int result = 1;
-		result += orderId.hashCode() * 37;
-		result += shoppingCart.hashCode() * 37;
-
+		result += 17 * orderId.hashCode();
+		result += 17 * shopper.getId().hashCode();
 		return result;
 	}
 
 	@Override
-	public String toString()
-	{
-		return "Order [orderId=" + orderId + ", shoppingCart=" + shoppingCart + ", customer=" + customer + ", isPayed=" + isPayed + "]";
+	public String toString() {
+		return "Order: " + "orderId: " + orderId + ", shopper " + shopper + ", orderTotal: " + orderTotal;
 	}
 }
--- Webshop_JPA/src/main/java/se/grouprich/webshop/model/ShoppingCart.java
+++ Webshop_JPA/src/main/java/se/grouprich/webshop/model/ShoppingCart.java
@@ -6,64 +6,67 @@ import java.util.List;
 
 import se.grouprich.webshop.exception.OrderException;
 
-public final class ShoppingCart implements Serializable
+public final class OrderRow implements Serializable
 {
 	private static final long serialVersionUID = 3865658878665558979L;
-	private final List<Product> products;
-	private double totalPrice;
+	private Product product;
+	private int quantity;
+	private double orderRowPrice;
 
-	public ShoppingCart()
-	{
-		products = new ArrayList<>();
-	}
 
-	public List<Product> getProducts()
-	{
-		return products;
-	}
 
-	public double getTotalPrice()
+        public OrderRow(Product product, int quantity)
+        {
+            this.product = product;
+            this.quantity = quantity;
+        }
+
+        public OrderRow(Product product)
+        {
+            this.product = product;
+            this.quantity = 1;
+        }
+
+        public Product getItem() {
+            return product;
+        }
+
+        public void setItem(Product item) {
+            this.product = item;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
+
+
+
+	public double orderRowPrice()
 	{
-		return totalPrice;
+		return orderRowPrice;
 	}
 	
-	public void setTotalPrice(final double totalPrice)
+	public void setTotalPrice(final double orderRowPrice)
 	{
-		this.totalPrice = totalPrice;
+		this.orderRowPrice = orderRowPrice;
 	}
 
-	public double calculateTotalPrice()
-	{
-		totalPrice = 0;
-		for (Product product : products)
-		{
-			totalPrice += (product.getPrice() * product.getOrderQuantity());
-		}
-		return totalPrice;
-	}
-	
-	public void addProductInShoppingCart(final Product product, final int orderQuantity) throws OrderException
-	{
-		products.add(product);
-		product.setOrderQuantity(orderQuantity);
-		calculateTotalPrice();
-	}
 
-	public void deleteOneProduct(final Product product) throws OrderException
-	{
-		if (!products.contains(product))
-		{
-			throw new OrderException("Product doesn't exsists.");
-		}
-		products.remove(product);
-		calculateTotalPrice();
-	}
+	/* Jag förstår inte värdet av att implementera equals och haschcode här. När spelar det roll om en orderrad är lik en annan?
+	10 000 users ska ju kunna ha exakt samma orderrad och det är ju inte relevant. Det vi inte vill är att samma user har flera
+	lika ordrar eller att det finns flera lika users eller flera lika products. Däremot vill vi ju inte ha flera ordrerrader
+	som är samma på en en order. T ex vill vi ju bara ha en rad med: 1 dator quantity 1 och om user lägger till en till dator vill
+	vi ju bara att quantity ska öka till 2. Fortfarande bara en rad, men det kollar vi lättast i addToOrder tror jag. Är ni med på
+	hur jag menar?
 
-	public void emptyShoppingCart(final List<Product> products)
-	{
-		products.removeAll(products);
-		calculateTotalPrice();
-	}
+	Samma med toString() tror jag i alla fall. En orderrad vet ju inte vilken order den tillhör, så det är nog aldrig relevant
+	att skriva ut en orderrad. Om man vill skriva ut en ordeerrad i en order så måste vi vet vilkeen order och det vet vi i
+	orderklassen..
 
 	@Override
 	public boolean equals(Object other)
@@ -90,9 +93,12 @@ public final class ShoppingCart implements Serializable
 		return result;
 	}
 
+
 	@Override
 	public String toString()
 	{
 		return "ShoppingCart [products=" + products + ", totalPrice=" + totalPrice + "]";
 	}
+
+	*/
 }
