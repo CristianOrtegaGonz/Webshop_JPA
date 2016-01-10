c059694454654a0acac1cf30d6d0c2189cb21c93
lindabjalla <lindabjalla@gmail.com>
2016-01-10 05:17:35 +0100

    Testat fetchOrderByUsername()

--- Webshop_JPA/src/main/java/se/grouprich/webshop/main/Main.java
+++ Webshop_JPA/src/main/java/se/grouprich/webshop/main/Main.java
@@ -8,9 +8,12 @@ import javax.persistence.Persistence;
 import se.grouprich.webshop.exception.OrderException;
 import se.grouprich.webshop.exception.PermissionException;
 import se.grouprich.webshop.exception.StorageException;
+import se.grouprich.webshop.model.Order;
+import se.grouprich.webshop.model.OrderRow;
 import se.grouprich.webshop.model.Product;
 import se.grouprich.webshop.model.Role;
 import se.grouprich.webshop.model.User;
+import se.grouprich.webshop.model.status.OrderStatus;
 import se.grouprich.webshop.repository.JpaOrderRepository;
 import se.grouprich.webshop.repository.JpaProductRepository;
 import se.grouprich.webshop.repository.JpaUserRepository;
@@ -50,10 +53,10 @@ public final class Main
 
 		Product product1 = eCommerceService.createProduct(admin, new Product("cat food", 33.657, 10));
 		Product product2 = eCommerceService.createProduct(admin, new Product("dog food", 27.83, 10));
-		Product product4 = eCommerceService.createProduct(admin, new Product("cat godis", 25.48, 10));
+		Product product3 = eCommerceService.createProduct(admin, new Product("cat godis", 25.48, 10));
 		System.out.println("product1 is: " + product1);
 		System.out.println("product2 is: " + product2);
-		System.out.println("product4 is: " + product4);
+		System.out.println("product3 is: " + product3);
 		System.out.println();
 
 		List<Product> catProducts = eCommerceService.searchProductsBasedOnProductName("cat");
@@ -61,6 +64,24 @@ public final class Main
 		System.out.println();
 		
 		List<Product> productsFood = eCommerceService.searchProductsBasedOnProductName("food");
-		System.out.println(productsFood);	
+		System.out.println("productsFood are: " + productsFood);	
+		
+		User userFetchedByUsername = eCommerceService.fetchUserByUsername(admin, "Kinoko");
+		System.out.println("userFetchedByUsername is: " + userFetchedByUsername);
+		System.out.println();
+		
+		Order order = eCommerceService.createOrder(customer2, new Order(customer2, new OrderRow(product1, 4)));
+		System.out.println("order is: " + order);
+		System.out.println();
+		
+		eCommerceService.addOrderRows(customer2, order, new OrderRow(product2, 2), new OrderRow(product3, 2));
+		Order updatedOrder = eCommerceService.updateOrder(admin, order);
+		System.out.println("updatedOrder is: " + updatedOrder);
+		System.out.println();
+		
+		Order fetchedOrder = eCommerceService.fetchOrderById(admin, 7L);
+		fetchedOrder.setStatus(OrderStatus.SHIPPED);
+		Order updatedOrder2 = eCommerceService.updateOrder(admin, fetchedOrder);
+		System.out.println("updatedOrder2 is: " + updatedOrder2);
 	}
 }
