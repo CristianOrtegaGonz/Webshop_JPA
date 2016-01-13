aae0c579ce853db2614a2d7ac025003bd87d4f05
lindabjalla <lindabjalla@gmail.com>
2016-01-13 07:08:36 +0100

    Refaktoriserat AbstractJpaRepository

--- Webshop_JPA/src/main/java/se/grouprich/webshop/model/status/OrderStatus.java
+++ Webshop_JPA/src/main/java/se/grouprich/webshop/model/status/OrderStatus.java
@@ -2,20 +2,8 @@ package se.grouprich.webshop.model.status;
 
 public enum OrderStatus
 {
-	PLACED("This mean ..."),
-	SHIPPED("Shipped is when you ... "),
-	PAYED("You have already payed ... "),
-	CANCELED("You have not finished the ... ");
-	
-	private final String description;
-	
-	OrderStatus(String description)
-	{
-		this.description = description;
-	}
-	
-	public String getDescription()
-	{
-		return description;
-	}
+	PLACED,
+	SHIPPED,
+	PAYED,
+	CANCELED;
 }
