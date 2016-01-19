433b3c9a2598a15e7b57f4f9f67b67d868c41c38
lindabjalla <lindabjalla@gmail.com>
2016-01-19 05:40:01 +0100

    bytte till genererad serialVersionUID

--- Webshop_JPA/src/main/java/se/grouprich/webshop/exception/RepositoryException.java
+++ Webshop_JPA/src/main/java/se/grouprich/webshop/exception/RepositoryException.java
@@ -2,8 +2,8 @@ package se.grouprich.webshop.exception;
 
 public final class RepositoryException extends RuntimeException
 {
-	private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = -8213862789105184537L;
+
 	public RepositoryException(String message)
 	{
 		super(message);
